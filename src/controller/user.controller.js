75308c2dbd809bf4d7ad481617af8d56a92b5f71
Suarez Matias <be.creativedesing@gmail.com>
2023-06-07 07:21:27 -0300

    integracion vista de formularios

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,3 +1,19 @@
-export const createUser = (req, res) => {
-    res.send({ status: "success", message: "Usuario creado" })
+import { userModel } from "./models/Users"
+
+export const createUser = async (req, res) => {
+    try {
+        const newUser = new userModel({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          gender: req.body.gender,
+          password: req.body.password,
+        });
+        await newUser.save();
+        res.redirect('/');
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Hubo un error al crear el usuario');
+      }
+  //  res.send({ status: "success", message: "Usuario creado" })
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,6 +4,11 @@ import passport from 'passport'
 
 const userRouter = Router()
 
+// GET endpoint para mostrar la vista de formulario 
+userRouter.get('/register', (req, res) => {
+    res.render('createUser');
+  });
+
 userRouter.post("/register", passport.authenticate("register"), createUser)
 
 export default userRouter
